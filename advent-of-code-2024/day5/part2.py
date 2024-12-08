5eea631bfc46bbfa5b0a5f1384828ee9408de47c
ricekot <git@ricekot.com>
2024-12-08 17:35:00 +0530

    Fix typo
    Signed-off-by: ricekot <git@ricekot.com>

--- advent-of-code-2024/day5/part2.py
+++ advent-of-code-2024/day5/part2.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import sys
 
 if len(sys.argv) != 2:
-    print("Usage: python part1.py input.txt")
+    print("Usage: python part2.py input.txt")
     sys.exit(1)
 
 input_file = Path(sys.argv[1])
